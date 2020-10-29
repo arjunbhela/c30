ec2274b78e7ef35b1814eeb8d1450ed2d6bac828
arjunbhela <arjunbhela123@gmail.com>
2020-10-28 20:02:24 -0700

    vanish pigs

--- sketch.js
+++ sketch.js
@@ -21,24 +21,24 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box1 = new Box(725,320,70,70);
+    box2 = new Box(895,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810,280,250, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    box3 = new Box(740,255,70,70);
+   box4 = new Box(880,255,70,70);
+    pig3 = new Pig(800, 280);
 
-    log3 =  new Log(810,180,300, PI/2);
+   log3 =  new Log(810,180,250, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+ box5 = new Box(810,160,60,60);
+   log4 = new Log(770,120,120, PI/7);
+   log5 = new Log(865,120,120, -PI/7);
 
     bird = new Bird(200,50);
 
-    //log6 = new Log(230,180,80, PI/2);
+    log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
@@ -52,14 +52,14 @@ function draw(){
     pig1.display();
     log1.display();
 
-    box3.display();
-    box4.display();
+   box3.display();
+   box4.display();
     pig3.display();
-    log3.display();
+   log3.display();
 
-    box5.display();
-    log4.display();
-    log5.display();
+  box5.display();
+   log4.display();
+   log5.display();
 
     bird.display();
     platform.display();
@@ -74,4 +74,11 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+        Matter.Body.setPosition(bird.body, {x:200,y:50})
+        slingshot.attach(bird.body);
+    }
 }
